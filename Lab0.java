e2f50af5561f1e917e3a90d29816cdadcd6c96e8
Janith D'Alwis <jdalwis@purdue.edu>
2024-08-22 13:42:13 -0400

    Add files via upload

---
+++ Lab0.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Lab0 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hi, My name is Janith.");
+        System.out.println("My major is Cybersecurity.");
+        System.out.println("My favorite Programming Language is Python.");
+    }
+}
